f255af2fc7eeb63fca779fc31253efe19382936d
AGNI RAJ BAIKANI <agnirajbaikani@AGNIs-MacBook-Pro.local>
2022-11-15 18:06:40 -0800

    Initial code check in

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DataPoisioning_FederatedLearning
-Analyzing Data Poisoing Attacks on Heterogeneous Federated Learning Systems
+# MNIST
+cd ./dataset
+python generate_mnist.py iid - - # for iid and unbalanced setting
+python generate_mnist.py iid balance - # for iid and balanced setting
+python generate_mnist.py noniid - pat # for pathological noniid and unbalanced setting
+python generate_mnist.py noniid - dir # for practical noniid and unbalanced setting
+
+
+# Train
+
+cd ./system
+python main.py -data mnist -m cnn -algo FedAvg -gr 100 -did 0 -go cnn # for FedAvg and MNIST
---
+++ system/main.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+import copy
+import torch
+import argparse
+import os
+import time
+import warnings
+import numpy as np
+from torch.nn.functional import dropout
+import torchvision
+
+from flcore.servers.serveravg import FedAvg
+from flcore.servers.serverperavg import PerAvg
+from flcore.servers.serverprox import FedProx
+from flcore.servers.serverlocal import Local
+from flcore.servers.serverproto import FedProto
+
+from flcore.trainmodel.models import *
+
+from flcore.trainmodel.bilstm import BiLSTM_TextClassification
+from utils.result_utils import average_data
+from utils.mem_utils import MemReporter
+
+warnings.simplefilter("ignore")
+torch.manual_seed(0)
+
+# hyper-params for Text tasks
+vocab_size = 98635
+max_len=200
+hidden_dim=32
+
+def run(args):
+
+    time_list = []
+    reporter = MemReporter()
+    model_str = args.model
+
+    for i in range(args.prev, args.times):
+        print(f"\n============= Running time: {i}th =============")
+        print("Creating server and clients ...")
+        start = time.time()
+
+        # Generate args.model
+        if model_str == "mlr":
+            if args.dataset == "mnist" or args.dataset == "fmnist":
+                args.model = Mclr_Logistic(1*28*28, num_classes=args.num_classes).to(args.device)
+            elif args.dataset == "Cifar10" or args.dataset == "Cifar100":
+                args.model = Mclr_Logistic(3*32*32, num_classes=args.num_classes).to(args.device)
+            else:
+                args.model = Mclr_Logistic(60, num_classes=args.num_classes).to(args.device)
+
+        elif model_str == "cnn":
+            if args.dataset == "mnist" or args.dataset == "fmnist":
+                args.model = FedAvgCNN(in_features=1, num_classes=args.num_classes, dim=1024).to(args.device)
+            elif args.dataset == "Cifar10" or args.dataset == "Cifar100":
+                args.model = FedAvgCNN(in_features=3, num_classes=args.num_classes, dim=1600).to(args.device)
+                # args.model = CifarNet(num_classes=args.num_classes).to(args.device)
+            elif args.dataset[:13] == "Tiny-imagenet" or args.dataset[:8] == "Imagenet":
+                args.model = FedAvgCNN(in_features=3, num_classes=args.num_classes, dim=10816).to(args.device)
+            else:
+                args.model = FedAvgCNN(in_features=3, num_classes=args.num_classes, dim=1600).to(args.device)
+
+
+        elif model_str == "dnn": # non-convex
+            if args.dataset == "mnist" or args.dataset == "fmnist":
+                args.model = DNN(1*28*28, 100, num_classes=args.num_classes).to(args.device)
+            elif args.dataset == "Cifar10" or args.dataset == "Cifar100":
+                args.model = DNN(3*32*32, 100, num_classes=args.num_classes).to(args.device)
+            else:
+                args.model = DNN(60, 20, num_classes=args.num_classes).to(args.device)
+        
+            
+        elif model_str == "lstm":
+            args.model = LSTMNet(hidden_dim=hidden_dim, vocab_size=vocab_size, num_classes=args.num_classes).to(args.device)
+
+        elif model_str == "bilstm":
+            args.model = BiLSTM_TextClassification(input_size=vocab_size, hidden_size=hidden_dim, output_size=args.num_classes, 
+                        num_layers=1, embedding_dropout=0, lstm_dropout=0, attention_dropout=0, 
+                        embedding_length=hidden_dim).to(args.device)
+
+        elif model_str == "fastText":
+            args.model = fastText(hidden_dim=hidden_dim, vocab_size=vocab_size, num_classes=args.num_classes).to(args.device)
+
+        elif model_str == "TextCNN":
+            args.model = TextCNN(hidden_dim=hidden_dim, max_len=max_len, vocab_size=vocab_size, 
+                            num_classes=args.num_classes).to(args.device)
+        else:
+            raise NotImplementedError
+
+        # select algorithm
+        if args.algorithm == "FedAvg":
+            server = FedAvg(args, i)
+
+        elif args.algorithm == "Local":
+            server = Local(args, i)
+
+        elif args.algorithm == "PerAvg":
+            server = PerAvg(args, i)
+
+        elif args.algorithm == "FedProx":
+            server = FedProx(args, i)
+
+        elif args.algorithm == "FedProto":
+            args.predictor = copy.deepcopy(args.model.fc)
+            args.model.fc = nn.Identity()
+            args.model = LocalModel(args.model, args.predictor)
+            server = FedProto(args, i)
+            
+        else:
+            raise NotImplementedError
+
+        server.train()
+
+        time_list.append(time.time()-start)
+
+    print(f"\nAverage time cost: {round(np.average(time_list), 2)}s.")
+    
+
+    # Global average
+    average_data(dataset=args.dataset, 
+                algorithm=args.algorithm, 
+                goal=args.goal, 
+                times=args.times, 
+                length=args.global_rounds/args.eval_gap+1)
+
+    print("All done!")
+
+    reporter.report()
+
+
+if __name__ == "__main__":
+    total_start = time.time()
+
+    parser = argparse.ArgumentParser()
+    # general
+    parser.add_argument('-go', "--goal", type=str, default="test", 
+                        help="The goal for this experiment")
+    parser.add_argument('-dev', "--device", type=str, default="cuda",
+                        choices=["cpu", "cuda"])
+    parser.add_argument('-did', "--device_id", type=str, default="0")
+    parser.add_argument('-data', "--dataset", type=str, default="mnist")
+    parser.add_argument('-nb', "--num_classes", type=int, default=10)
+    parser.add_argument('-m', "--model", type=str, default="cnn")
+    parser.add_argument('-p', "--predictor", type=str, default="cnn")
+    parser.add_argument('-lbs', "--batch_size", type=int, default=10)
+    parser.add_argument('-lr', "--local_learning_rate", type=float, default=0.005,
+                        help="Local learning rate")
+    parser.add_argument('-gr', "--global_rounds", type=int, default=1000)
+    parser.add_argument('-ls', "--local_steps", type=int, default=1)
+    parser.add_argument('-algo', "--algorithm", type=str, default="FedAvg")
+    parser.add_argument('-jr', "--join_ratio", type=float, default=1.0,
+                        help="Ratio of clients per round")
+    parser.add_argument('-nc', "--num_clients", type=int, default=20,
+                        help="Total number of clients")
+    parser.add_argument('-pv', "--prev", type=int, default=0,
+                        help="Previous Running times")
+    parser.add_argument('-t', "--times", type=int, default=1,
+                        help="Running times")
+    parser.add_argument('-eg', "--eval_gap", type=int, default=1,
+                        help="Rounds gap for evaluation")
+    parser.add_argument('-dp', "--privacy", type=bool, default=False,
+                        help="differential privacy")
+    parser.add_argument('-dps', "--dp_sigma", type=float, default=0.0)
+    parser.add_argument('-sfn', "--save_folder_name", type=str, default='models')
+    # practical
+    parser.add_argument('-cdr', "--client_drop_rate", type=float, default=0.0,
+                        help="Dropout rate for clients")
+    parser.add_argument('-tsr', "--train_slow_rate", type=float, default=0.0,
+                        help="The rate for slow clients when training locally")
+    parser.add_argument('-ssr', "--send_slow_rate", type=float, default=0.0,
+                        help="The rate for slow clients when sending global model")
+    parser.add_argument('-ts', "--time_select", type=bool, default=False,
+                        help="Whether to group and select clients at each round according to time cost")
+    parser.add_argument('-tth', "--time_threthold", type=float, default=10000,
+                        help="The threthold for droping slow clients")
+    # pFedMe / PerAvg / FedProx / FedAMP / FedPHP
+    parser.add_argument('-bt', "--beta", type=float, default=0.0,
+                        help="Average moving parameter for pFedMe, Second learning rate of Per-FedAvg, \
+                        or L1 regularization weight of FedTransfer")
+    parser.add_argument('-lam', "--lamda", type=float, default=1.0,
+                        help="Regularization weight for pFedMe and FedAMP")
+    parser.add_argument('-mu', "--mu", type=float, default=0,
+                        help="Proximal rate for FedProx")
+    parser.add_argument('-K', "--K", type=int, default=5,
+                        help="Number of personalized training steps for pFedMe")
+    parser.add_argument('-lrp', "--p_learning_rate", type=float, default=0.01,
+                        help="personalized learning rate to caculate theta aproximately using K steps")
+
+    args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.device_id
+
+    if args.device == "cuda" and not torch.cuda.is_available():
+        print("\ncuda is not avaiable.\n")
+        args.device = "cpu"
+
+    print("=" * 50)
+
+    print("Algorithm: {}".format(args.algorithm))
+    print("Local batch size: {}".format(args.batch_size))
+    print("Local steps: {}".format(args.local_steps))
+    print("Local learing rate: {}".format(args.local_learning_rate))
+    print("Total number of clients: {}".format(args.num_clients))
+    print("Clients join in each round: {}".format(args.join_ratio))
+    print("Client drop rate: {}".format(args.client_drop_rate))
+    print("Time select: {}".format(args.time_select))
+    print("Time threthold: {}".format(args.time_threthold))
+    print("Global rounds: {}".format(args.global_rounds))
+    print("Running times: {}".format(args.times))
+    print("Dataset: {}".format(args.dataset))
+    print("Local model: {}".format(args.model))
+    print("Using device: {}".format(args.device))
+
+    if args.device == "cuda":
+        print("Cuda device id: {}".format(os.environ["CUDA_VISIBLE_DEVICES"]))
+    print("=" * 50)
+
+    run(args)
